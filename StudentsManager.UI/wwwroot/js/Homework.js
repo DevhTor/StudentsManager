b425b34d0a00f562f93c0576b0816072257b4015
Devhtor <devhtor@gmail.com>
2025-08-24 09:38:56 -0400

    separate some javascritp

---
+++ StudentsManager.UI/wwwroot/js/Homework.js
@@ -0,0 +1,18 @@
+﻿// Lógica para el modal de confirmación de eliminación
+var confirmDeleteModal = document.getElementById('confirmDeleteModal');
+
+confirmDeleteModal.addEventListener('show.bs.modal', function (event) {
+    // Botón que activó el modal
+    var button = event.relatedTarget;
+    // Extrae la información de los atributos data-
+    var homeworkId = button.getAttribute('data-id');
+    var homeworkDescription = button.getAttribute('data-description');
+
+    // Actualiza el input oculto en el formulario del modal
+    var modalInput = confirmDeleteModal.querySelector('#homeworkIdInput');
+    modalInput.value = homeworkId;
+
+    // Actualiza la descripción en el cuerpo del modal
+    var modalDescriptionSpan = confirmDeleteModal.querySelector('#homeworkDescriptionSpan');
+    modalDescriptionSpan.textContent = homeworkDescription;
+});
---
+++ StudentsManager.UI/wwwroot/js/Student.js
@@ -0,0 +1,19 @@
+﻿
+// Lógica para el modal de confirmación de eliminación
+var confirmDeleteModal = document.getElementById('confirmDeleteModal');
+
+confirmDeleteModal.addEventListener('show.bs.modal', function (event) {
+    // Botón que activó el modal
+    var button = event.relatedTarget;
+    // Extrae la información de los atributos data-
+    var studentId = button.getAttribute('data-id');
+    var studentName = button.getAttribute('data-name');
+
+    // Actualiza el input oculto en el formulario del modal
+    var modalInput = confirmDeleteModal.querySelector('#studentIdInput');
+    modalInput.value = studentId;
+
+    // Actualiza la descripción en el cuerpo del modal
+    var modalNameSpan = confirmDeleteModal.querySelector('#studentNameSpan');
+    modalNameSpan.textContent = studentName;
+});
